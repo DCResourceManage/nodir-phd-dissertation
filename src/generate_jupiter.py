ba79f74941265ed8209acc7da0b51ac64b057a29
Nodir Kodirov <nodir.qodirov@gmail.com>
2022-06-01 20:38:57 +0000

    minor typo fix

--- src/generate_jupiter.py
+++ src/generate_jupiter.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-""" Generate Juputer datacenter topology. """
+""" Generate Jupiter datacenter topology. """
 
 import argparse
 import json
